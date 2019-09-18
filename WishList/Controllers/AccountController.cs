fe47771290f841c1828753c4acf044f77ab2d690
ONAKOYA OLAMIDE <olamideonakoya1@gmail.com>
2019-09-18 11:54:27 +0100

    initial Commit

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+using WishList.Models.AccountViewModels;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager )
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel viewModel)
+        {
+            
+            if (ModelState.IsValid)
+            { var password = viewModel.Password;
+                ApplicationUser user = new ApplicationUser
+                {
+                    UserName = viewModel.Email,
+                    Email = viewModel.Email
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(user, password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+
+                }
+                foreach (var Error in result.Errors)
+                {
+                    ModelState.AddModelError("Password", Error.Description);
+                }
+            }
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [Compare("Password",ErrorMessage ="Password do not match")]
+        [DataType(DataType.Password)]
+        public string  ConfirmPaasword { get; set; }
+    }
+}
